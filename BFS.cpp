b4a495c97f8855965f1e9029f5aae58b79397a28
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2025-10-02 23:02:05 +0200

    Adding comments about the structure of BFS

--- BFS.cpp
+++ BFS.cpp
@@ -3,6 +3,46 @@
 Problem: Shortest path in binary matrix.
 
 
+STEPS
+
+
+1 - We include vectors since we're dealing with a grid or NxN matrix
+2 - The if statement checks if start or end is blocked meaning it checks
+    if the first element and last element in the grid is a 0 or 1 and if
+    it is a 1, the program must return -1 because 1 signals a bocked cell
+
+    We return -1 because it says we can't begin moving because of a blocked
+    cell (1)
+
+3 - The directions vector stores all possible directions which are 8 (left,
+    right, up, down, diagonal left, diagonal right, diagonal up and diagonal
+    down). Each pair represents (row_change, column_change_
+
+4 - The queue stores the row, column, distance. Since the first element is
+    the start (0, 0), we say its distance = 1
+
+    Visited is a vector matrix with a size n x n to keep track of whether each
+    cell has been visited or not and since it says false, each cell hasn't been
+    visited
+
+    visited = [
+        false, false,
+        false, false
+    ]
+
+    visited[0][0] = true says the first element has visited the first cell
+    visited = [
+        true, false,
+        false, false
+    ]
+
+5 - The while loop only loops when the queue is not empty and since it has
+    values of the first element and its distance, its going to loop
+    
+
+
+
+
 
 */
 
