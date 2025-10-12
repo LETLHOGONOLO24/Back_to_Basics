db0c6421ce33dbc9ba370a6190742812e611fdf7
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2025-10-12 23:35:18 +0200

    Added example 1 of the merge interval

--- MergeInterval.cpp
+++ MergeInterval.cpp
@@ -9,6 +9,18 @@ intervals[i] = [start_i, end_i].
 Merge all overlapping intervals and return an array of the non-
 overlapping intervals that cover all the intervals in the input.
 
+
+Example 1:
+
+Input:
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+
+Output:
+[[1,6],[8,10],[15,18]]
+
+Explanation:
+Intervals [1,3] and [2,6] overlap → merged into [1,6].
+
 */
 
 #include <iostream>
