fe8bcaa9a0eaefe69fce047a7c20cc615a16b7e1
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2025-10-19 21:20:19 +0200

    Added the steps of how the code runs

--- InPlaceReversal.cpp
+++ InPlaceReversal.cpp
@@ -16,7 +16,21 @@ Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
 STEPS
 
 
-1 - 
+1 - prev keeps track of the previous node (starts as NULL since the
+    first node will become the last)
+    - current starts at the head node
+
+    - next temporarily stores the next node before we change pointers
+
+2 - while (current != NULL) { We repeat this for every node until
+    current becomes NULL.
+    - next = current->next; next is the 2nd node or the node next to the
+    current
+
+    - current->next = prev; we're saying the pointer that is pointing to
+    next is going to point to previous which is null
+    - prev = current; we're 
+    - current = next; we say current is the next node
 
 
 */
